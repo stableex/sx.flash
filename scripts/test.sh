d396a846850504e6b210d1ac1e894f2c8417c439
Denis <carriere.denis@gmail.com>
2020-06-08 11:12:57 -0400

    add tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,3 +5,7 @@ cleos push action basic init '["1.0000 EOS"]' -p basic
 
 # callback
 cleos push action callback init '["myaccount", "1.0000 EOS"]' -p callback
+
+# save & check
+cleos push action flash.sx savebalance '["myaccount", [{"key": "EOS", "value": "eosio.token"}]]' -p myaccount
+cleos push action flash.sx checkbalance '["myaccount", [{"key": "EOS", "value": "eosio.token"}]]' -p myaccount
